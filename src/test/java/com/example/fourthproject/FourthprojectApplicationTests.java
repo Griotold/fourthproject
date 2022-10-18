e60909b70eaf63c8bde31f29770fa49009209599
Griotold <101307758+Griotold@users.noreply.github.com>
2022-10-18 12:43:08 +0900

    헬로 + dto 테스트

--- src/test/java/com/example/fourthproject/FourthprojectApplicationTests.java
+++ src/test/java/com/example/fourthproject/FourthprojectApplicationTests.java
@@ -1,6 +1,6 @@
 package com.example.fourthproject;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
---
+++ src/test/java/com/example/fourthproject/web/HelloControllerTest.java
@@ -0,0 +1,45 @@
+package com.example.fourthproject.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = HelloController.class)
+public class HelloControllerTest {
+
+    @Autowired            // 스프링 MVC 테스트의 시작점
+    private MockMvc mvc; // 웹 API를 테스트할 떄 사용한다
+
+    @Test
+    public void hello가_리턴된다() throws Exception {
+        String hello = "hello";
+
+        mvc.perform(get("/hello")) // HTTP GET 요청 // 체이닝 지원으로 여러 검증 기능 이어서 선언
+                .andExpect(status().isOk()) // 200인지 아닌지 검증
+                .andExpect(content().string(hello)); // 응답 바디의 내용 검증
+    }
+
+    @Test
+    public void helloDto가_리턴된다() throws Exception {
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(
+                    get("/hello/dto")
+                            .param("name", name)
+                            .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
+    }
+}
